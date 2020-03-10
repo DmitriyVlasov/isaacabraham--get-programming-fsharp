ada4bdbb916da4b9b3ba2d9aed98d834e8baa999
Dmitriy Vlasov <vlasovde@gmail.com>
2020-03-10 11:44:17 +0300

    Extract base path in Auditing file

--- src/code-listings/lesson-14/Auditing.fs
+++ src/code-listings/lesson-14/Auditing.fs
@@ -3,11 +3,18 @@ module Capstone2.Auditing
 open Capstone2.Domain
 open System.IO
 
+/// Base Path
+[<Literal>]
+let BASE_PATH = @"D:\temp\getprogrammingwithfsharp\lesson14"
+
 /// Logs to the console
 let console account message = printfn "Account %O: %s" account.AccountId message
 
 /// Logs to the file system
 let fileSystem account message =
-    Directory.CreateDirectory(sprintf @"C:\temp\getprogrammingwithfsharp\lesson14\%s" account.Owner.Name) |> ignore
-    let filePath = sprintf @"C:\temp\getprogrammingwithfsharp\lesson14\%s\%O.txt" account.Owner.Name account.AccountId
+    sprintf @"%s\%s" BASE_PATH account.Owner.Name
+    |> Directory.CreateDirectory 
+    |> ignore
+    let filePath = 
+        sprintf @"%s\%s\%O.txt" BASE_PATH account.Owner.Name account.AccountId
     File.AppendAllLines(filePath, [ message ])
